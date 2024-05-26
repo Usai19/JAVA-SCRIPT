7e2a70e8dc1964d92f04caf1c503bbcabe428d5e
Usai19 <139556074+Usai19@users.noreply.github.com>
2024-05-26 22:47:47 +0600

    Update main.js
    // CAL BACK FUNCTION

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-main.js
+// CAL BACK FUNCTION
+
+
+function getFullname(func) {
+    return func()
+}
+
+function myName() {
+    return "My Name is" + " " + "Usai Mong" + " " + "Marma"
+}
+
+console.log(myName(getFullname))
+console.log(getFullname(myName))
